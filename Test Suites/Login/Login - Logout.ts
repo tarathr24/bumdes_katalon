0c5fb49505befc00addf264b10cf3ba7ef98be35
BRI <BRI@DESKTOP-BI4D3KH>
2021-05-17 10:16:46 +0700

    testing

---
+++ Test Suites/Login/Login - Logout.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login - Logout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e142bec8-e1d6-4ca9-a108-1ce680da4a8c</testSuiteGuid>
+   <testCaseLink>
+      <guid>28ab6329-60bc-494e-bf52-2f342a1cf92a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
